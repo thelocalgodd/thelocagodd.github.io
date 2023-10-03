818c3943a3b918a20b4d55e567058b7d6e305a41
thelocalgodd <vkkpemlie@st.knust.edu.gh>
2023-10-03 19:29:04 +0000

    improvements all round

--- script.js
+++ script.js
@@ -5,14 +5,24 @@ function darkmode() {
     element.classList.toggle("dark-mode");
 
     if (isDarkMode) {
-        document.getElementById("dark").innerHTML = "dark mode";
+        document.getElementById("dark").innerHTML = "dark_mode";
     } else {
-    document.getElementById("dark").innerHTML = "light mode";
+        document.getElementById("dark").innerHTML = "light_mode";
     }
-
     isDarkMode = !isDarkMode;
 }
 
+let timeofday = new Date();
 
-    const time = new Date();
-    document.getElementById('timep').innerHTML = time.getTime()
+hours = timeofday.getHours()
+
+if (hours >= 0 && hours < 12) {
+    console.log('Good Morning')
+    document.getElementById('greeting').innerHTML = `Good Morning, Welcome to my Site.`
+} else if (hours >= 12 && hours <= 16) {
+    document.getElementById('greeting').innerHTML = `Good Afternoon, Welcome to my Site.`
+    console.log('Good Afternoon')
+} else {
+    document.getElementById('greeting').innerHTML = `Good Evening, Welcome to my Site.`
+    console.log('Good Evening')
+}
