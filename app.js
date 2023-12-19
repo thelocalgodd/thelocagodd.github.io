5a5d3bab2f8bfa4cd207cf203e8961eb9b9c5258
thelocalgodd <vkkpemlie@st.knust.edu.gh>
2023-12-19 00:08:31 +0000

    .

--- app.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('resize', function() {
-    if (window.innerWidth <= 900) {
-        window.location.href = "../pages/mobilepage.html"; 
-    } 
-    if (window.innerWidth > 900) {
-        window.location.href = "../pages/projects.html"; 
-    }
-});
